53e3a2ce37ae252fc1a34b8d863b13786c01853c
Kosholadka <kkrus.9725@gmail.com>
2024-09-13 14:59:40 +0200

    build: add basename in React Router for proper routing on GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,45 +13,50 @@ import './index.css'
 import { useAuth } from './stores'
 import { Layout, ProtectedRoute } from './uikit'
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: 'signup',
+          element: <SignUp />,
+        },
+        {
+          path: 'login',
+          element: <LogIn />,
+        },
+        {
+          path: 'profile',
+          element: (
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          ),
+          children: [
+            {
+              path: 'settings',
+              element: <Settings />,
+            },
+          ],
+        },
+        {
+          path: 'cart',
+          element: <Cart />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: 'signup',
-        element: <SignUp />,
-      },
-      {
-        path: 'login',
-        element: <LogIn />,
-      },
-      {
-        path: 'profile',
-        element: (
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        ),
-        children: [
-          {
-            path: 'settings',
-            element: <Settings />,
-          },
-        ],
-      },
-      {
-        path: 'cart',
-        element: <Cart />,
-      },
-    ],
-  },
-])
+    basename: '/picnic-app',
+  }
+)
 
 export const App = () => {
   useAuth()
