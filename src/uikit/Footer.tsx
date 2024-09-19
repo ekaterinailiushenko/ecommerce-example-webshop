70ff66791e96cfae8d422e49225201c5d8ccb34c
Kosholadka <kkrus.9725@gmail.com>
2024-09-19 14:56:11 +0200

    feat: create Footer component and display build version

---
+++ src/uikit/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div className="flex justify-center bg-gray-100">
+      <p className="text-xs p-4 text-gray-500">Version: {__APP_VERSION__}</p>
+    </div>
+  )
+}
