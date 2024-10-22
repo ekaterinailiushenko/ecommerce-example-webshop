a704c473d1237cf4b0b4401b2ea7132e915a1609
Kosholadka <kkrus.9725@gmail.com>
2024-10-22 14:56:24 +0200

    chor: change base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/picnic-app/',
+  base: '/picnic-app',
   plugins: [react()],
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
