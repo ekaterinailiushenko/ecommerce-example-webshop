1c07a0a22ba3b3bda3db5ed714e7188829051b62
Kosholadka <kkrus.9725@gmail.com>
2024-11-07 09:07:20 +0100

    refactor: add goToHomepage function

--- src/uikit/Header.tsx
+++ src/uikit/Header.tsx
@@ -1,26 +1,19 @@
-import { Link, useMatch } from 'react-router-dom'
-
 import { Input } from './Input'
 import en from '../i18n/en.json'
 import { NavMenu } from './NavMenu'
 import { HOME_PAGE_URL } from '../helpers/constants'
-import { refreshPage } from '../utilities/refreshPage'
 import picnicHeaderLogo from '../assets/picnicHeaderLogo.png'
 
 export const Header = () => {
-  const HomePage = useMatch(HOME_PAGE_URL)
-
-  const handleRefreshPage = () => {
-    if (HomePage) {
-      refreshPage()
-    }
+  const goToHomepage = () => {
+    window.location.replace(HOME_PAGE_URL)
   }
 
   return (
     <header className="flex justify-between items-center bg-header">
-      <Link to={HOME_PAGE_URL} onClick={handleRefreshPage}>
+      <button onClick={goToHomepage}>
         <img className="size-16" src={picnicHeaderLogo} alt={en.header.logoAltText} />
-      </Link>
+      </button>
       <Input />
       <NavMenu />
     </header>
