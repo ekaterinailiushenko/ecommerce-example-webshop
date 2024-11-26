2cc8b67d6d370873c89edaf21a7601874d191c44
Kosholadka <kkrus.9725@gmail.com>
2024-11-26 16:32:42 +0100

    refactor: replace link in EmptyCart component

--- src/pages/Cart/components/EmptyCart/index.tsx
+++ src/pages/Cart/components/EmptyCart/index.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom'
 
 import en from '../../../../i18n/en.json'
 import { Button } from '../../../../uikit'
+import { Routes } from '../../../../router/config'
 import emptyCartLogo from '../../../../assets/emptyCartLogo.png'
 
 export const EmptyCart = () => (
@@ -9,7 +10,7 @@ export const EmptyCart = () => (
     <img src={emptyCartLogo} alt={en.cart.emptyCart.altText} className="size-32" />
     <p className="font-bold text-2xl">{en.cart.emptyCart.mainMessage}</p>
     <p className="text-md text-slate-600">{en.cart.emptyCart.nextMessage}</p>
-    <Link to={`/`}>
+    <Link to={Routes.HOME_PAGE_URL}>
       <Button variant="success" label={en.cart.emptyCart.linkToMainPage} size="regular" />
     </Link>
   </div>
