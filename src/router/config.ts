c5e1a1678b185806157b0375fcf61dc7d46ba02c
Kosholadka <kkrus.9725@gmail.com>
2024-12-13 13:04:47 +0100

    infra: update repository links

--- src/router/config.ts
+++ src/router/config.ts
@@ -3,7 +3,7 @@ export enum Routes {
   CART_PAGE_URL = '/cart',
   LOGIN_PAGE_URL = '/login',
   SIGNUP_PAGE_URL = '/signup',
-  BASENAME_URL = '/picnic-app',
+  BASENAME_URL = '/ecommerce-example-webshop',
   PROFILE_PAGE_URL = '/profile',
   SETTINGS_PAGE_URL = 'settings',
 }
