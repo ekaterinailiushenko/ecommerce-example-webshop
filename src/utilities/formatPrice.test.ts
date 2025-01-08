66b97d0dd29b298a1d3c45ce6d342a3d6321e467
Kosholadka <kkrus.9725@gmail.com>
2025-01-08 11:17:06 +0100

    test: add test for formatPrice utility function

---
+++ src/utilities/formatPrice.test.ts
@@ -0,0 +1,23 @@
+import { formatPrice } from './formatPrice'
+
+describe('formatPrice', () => {
+  it('should format 0 correctly', () => {
+    expect(formatPrice(0)).toEqual('0,00 €')
+  })
+
+  it('should format a large number correctly', () => {
+    expect(formatPrice(123456789)).toEqual('1.234.567,89 €')
+  })
+
+  it('should format a negative number correctly', () => {
+    expect(formatPrice(-123)).toEqual('-1,23 €')
+  })
+
+  it('should format a small positive number correctly', () => {
+    expect(formatPrice(84)).toEqual('0,84 €')
+  })
+
+  it('should return undefined if price is not an integer', () => {
+    expect(formatPrice(125.34)).toBeUndefined()
+  })
+})
