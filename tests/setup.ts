69e90e35662e28d4e7781a907f3018562cd7478a
ekaterinailiushenko <kkrus.9725@gmail.com>
2025-01-21 11:48:16 +0100

    test: add cartApi mock in tests setup

--- tests/setup.ts
+++ tests/setup.ts
@@ -3,9 +3,24 @@ import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import failOnConsole from 'vitest-fail-on-console'
 
+import type * as CartApiModule from '../src/api/cartApi'
+
 failOnConsole()
 
 afterEach(() => {
   cleanup()
   vi.clearAllMocks()
+})
+
+vi.mock('../src/api/cartApi', () => {
+  const mock: typeof CartApiModule = {
+    cartApi: {
+      clearCart: vi.fn(),
+      getCartSummary: vi.fn(),
+      addProductToCart: vi.fn(),
+      deleteProductFromCart: vi.fn(),
+    },
+  }
+
+  return mock
 })
