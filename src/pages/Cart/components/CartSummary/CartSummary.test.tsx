7126240a610f04be8865a422b3bb5c3ff9db0b73
ekaterinailiushenko <kkrus.9725@gmail.com>
2025-01-22 15:14:22 +0100

    test: add tests for CartSummary component

---
+++ src/pages/Cart/components/CartSummary/CartSummary.test.tsx
@@ -0,0 +1,56 @@
+import { act, render, screen, within } from '@testing-library/react'
+
+import { CartSummary } from './index'
+import en from '../../../../i18n/en.json'
+import { cartApi } from '../../../../api/cartApi'
+import { mockCartSummary } from '../../../../mocks'
+import { flushPromises, formatPrice } from '../../../../utilities'
+import { CartContextProvider } from '../../../../contexts/CartContext/provider'
+import { AuthContextProvider } from '../../../../contexts/AuthContext/provider'
+
+const renderCartSummary = async () => {
+  render(
+    <AuthContextProvider>
+      <CartContextProvider>
+        <CartSummary />
+      </CartContextProvider>
+    </AuthContextProvider>
+  )
+
+  await act(async () => {
+    await flushPromises()
+  })
+}
+
+describe('CartSummary', () => {
+  beforeEach(() => {
+    vi.mocked(cartApi.getCartSummary).mockResolvedValue(mockCartSummary)
+  })
+
+  it('should display cart summary title, product quantity, calculated prices and action button', async () => {
+    await renderCartSummary()
+
+    const cartSummary = screen.getByTestId('cart-summary')
+
+    expect(within(cartSummary).getByText(en.cart.summary)).toBeVisible()
+    expect(
+      within(cartSummary).getByText(`(${mockCartSummary.productsQuantity} ${en.cart.productItems})`)
+    ).toBeVisible()
+
+    const assertPriceInCartSummary = (price: number) => {
+      const formattedPrice = formatPrice(price)
+
+      if (formattedPrice) {
+        expect(within(cartSummary).findByText(formattedPrice)).not.toBeNull()
+      }
+    }
+
+    assertPriceInCartSummary(mockCartSummary.totalPrice)
+    assertPriceInCartSummary(mockCartSummary.deliveryCosts)
+    assertPriceInCartSummary(mockCartSummary.totalPriceWithDeliveryCosts)
+
+    expect(
+      within(cartSummary).getByRole('button', { name: en.cart.buttons.buyNow.title })
+    ).toBeVisible()
+  })
+})
