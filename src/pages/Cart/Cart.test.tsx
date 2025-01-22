97a09e8d132e6cc7515fe2180dcb44be31a04c28
ekaterinailiushenko <kkrus.9725@gmail.com>
2025-01-22 15:18:23 +0100

    test: remove children components tests

--- src/pages/Cart/Cart.test.tsx
+++ src/pages/Cart/Cart.test.tsx
@@ -1,62 +1,23 @@
-import { Router } from 'react-router'
-import { createMemoryHistory } from 'history'
+import { MemoryRouter } from 'react-router'
 import { screen, render, act, within } from '@testing-library/react'
 
 import { Cart } from './index'
 import en from '../../i18n/en.json'
 import { cartApi } from '../../api/cartApi'
-import { Routes } from '../../router/config'
-import type { Cart as CartType } from '../../api/types'
-import { flushPromises, formatPrice } from '../../utilities'
+import { flushPromises } from '../../utilities'
 import { CartContextProvider } from '../../contexts/CartContext/provider'
 import { AuthContextProvider } from '../../contexts/AuthContext/provider'
-
-const mockProducts: CartType['products'] = [
-  {
-    product_id: '1',
-    name: 'Apples',
-    pricePerProduct: 120,
-    image: 'https://s3-eu-west-1.amazonaws.com/developer-application-test/images/1.jpg',
-    amountInCart: 1,
-    priceForAmountInCart: 0,
-  },
-  {
-    product_id: '2',
-    name: 'Oranges',
-    pricePerProduct: 167,
-    image: 'https://s3-eu-west-1.amazonaws.com/developer-application-test/images/2.jpg',
-    amountInCart: 1,
-    priceForAmountInCart: 0,
-  },
-]
-
-const mockCartSummary: CartType = {
-  products: mockProducts,
-  totalPrice: 3505,
-  deliveryCosts: 500,
-  productsQuantity: 2,
-  totalPriceWithDeliveryCosts: 4005,
-}
-
-const mockEmptyCartSummary: CartType = {
-  products: [],
-  totalPrice: 0,
-  deliveryCosts: 0,
-  productsQuantity: 0,
-  totalPriceWithDeliveryCosts: 0,
-}
-
-const history = createMemoryHistory()
+import { mockCartSummary, mockEmptyCartSummary, mockProducts } from '../../mocks'
 
 export const renderApp = async () => {
   render(
-    <Router location={history.location} navigator={history}>
+    <MemoryRouter>
       <AuthContextProvider>
         <CartContextProvider>
           <Cart />
         </CartContextProvider>
       </AuthContextProvider>
-    </Router>
+    </MemoryRouter>
   )
 
   await act(async () => {
@@ -76,22 +37,6 @@ describe('Cart page', () => {
       expect(screen.queryByTestId('cart-header')).toBeNull()
       expect(screen.queryByTestId('cart-summary')).toBeNull()
     })
-
-    it('should display a navigation button that redirects to homepage when clicked', async () => {
-      await renderApp()
-
-      const startShoppingButton = within(screen.getByTestId('empty-cart')).getByRole('button', {
-        name: en.cart.emptyCart.linkToMainPage,
-      })
-
-      await act(async () => {
-        startShoppingButton.click()
-
-        await flushPromises()
-      })
-
-      expect(history.location.pathname).toBe(Routes.HOME_PAGE_URL)
-    })
   })
 
   describe('when the cart has items', () => {
@@ -100,14 +45,11 @@ describe('Cart page', () => {
       vi.mocked(cartApi.clearCart).mockResolvedValue(mockEmptyCartSummary)
     })
 
-    it('should render cart header with correct product quantity and not display empty cart section', async () => {
+    it('should render cart header, cart summary and not display empty cart section', async () => {
       await renderApp()
 
-      expect(
-        within(screen.getByTestId('cart-header')).getByText(
-          `(${mockCartSummary.productsQuantity} ${en.cart.productItems})`
-        )
-      ).toBeVisible()
+      expect(screen.getByTestId('cart-header')).toBeVisible()
+      expect(screen.getByTestId('cart-summary')).toBeVisible()
 
       expect(screen.queryByTestId('empty-cart')).toBeNull()
     })
@@ -116,30 +58,6 @@ describe('Cart page', () => {
       await renderApp()
 
       mockProducts.forEach(product => expect(screen.getByText(product.name)).toBeVisible())
-    })
-
-    it('should render cart summary with calculated prices and product quantity', async () => {
-      await renderApp()
-
-      const cartSummary = screen.getByTestId('cart-summary')
-
-      expect(
-        within(cartSummary).getByText(
-          `(${mockCartSummary.productsQuantity} ${en.cart.productItems})`
-        )
-      ).toBeVisible()
-
-      const assertPriceInCartSummary = (price: number) => {
-        const formattedPrice = formatPrice(price)
-
-        if (formattedPrice) {
-          expect(within(cartSummary).findByText(formattedPrice)).not.toBeNull()
-        }
-      }
-
-      assertPriceInCartSummary(mockCartSummary.totalPrice)
-      assertPriceInCartSummary(mockCartSummary.deliveryCosts)
-      assertPriceInCartSummary(mockCartSummary.totalPriceWithDeliveryCosts)
     })
 
     it('should clear all products and display empty cart section after clear cart button is clicked', async () => {
