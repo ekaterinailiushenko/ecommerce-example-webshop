6142803a65e8ec9d1db3225682cc26f59d0ce747
ekaterinailiushenko <kkrus.9725@gmail.com>
2025-02-05 16:27:09 +0100

    feature: add german translations

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,18 +1,22 @@
-import i18next from 'i18next'
+import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import detector from 'i18next-browser-languagedetector'
 
-import en from './en.json'
+import en from './locales/en/translation.json'
+import de from './locales/de/translation.json'
 
-const resources = {
-  en: { translation: en },
+export const resources = {
+  English: { translation: en },
+  Deutsch: { translation: de },
 }
 
-await i18next.use(initReactI18next).init({
-  resources,
-  lng: 'en',
-  fallbackLng: 'en',
-  supportedLngs: ['en'],
-  preload: ['en'],
-})
+await i18n
+  .use(detector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'English',
+    supportedLngs: Object.keys(resources),
+  })
 
-export default i18next
+export default i18n
