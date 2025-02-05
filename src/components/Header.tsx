16c3bb65c219369de11c2e711af32e6d3f6250bc
ekaterinailiushenko <kkrus.9725@gmail.com>
2025-02-05 16:30:37 +0100

    feature: add switch language button with dropdown

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next'
 import { Input } from './Input'
 import { NavMenu } from './NavMenu'
 import { Routes } from '../router/config'
+import { LocaleSwitcher } from './LocaleSwitcher'
 import picnicHeaderLogo from '../assets/picnicHeaderLogo.png'
 
 export const Header = () => {
@@ -25,6 +26,7 @@ export const Header = () => {
           </button>
           <Input />
         </div>
+        <LocaleSwitcher />
         <div className="hidden md:block">
           <NavMenu />
         </div>
---
+++ src/components/LocaleSwitcher.tsx
@@ -0,0 +1,38 @@
+import { useTranslation } from 'react-i18next'
+import { ChevronDownIcon } from '@heroicons/react/24/outline'
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+
+import { resources } from '../i18n/config'
+
+export const LocaleSwitcher = () => {
+  const { i18n } = useTranslation()
+
+  return (
+    <div className="relative inline-block text-left">
+      <Menu>
+        <MenuButton className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none">
+          {i18n.language}
+          <ChevronDownIcon className="w-5 h-5 ml-2" aria-hidden="true" />
+        </MenuButton>
+
+        <MenuItems
+          transition
+          className="absolute right-0 mt-1 p-1 w-40 space-y-1 rounded-xl shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none transition duration-100 ease-out data-[closed]:scale-95 data-[closed]:opacity-0"
+        >
+          {Object.keys(resources)
+            .filter(locale => locale !== i18n.language)
+            .map(locale => (
+              <MenuItem key={locale}>
+                <button
+                  onClick={() => i18n.changeLanguage(locale)}
+                  className="rounded-lg data-[focus]:bg-gray-100 data-[focus]:text-gray-900 text-gray-700 block w-full text-left px-4 py-2 text-sm"
+                >
+                  {locale}
+                </button>
+              </MenuItem>
+            ))}
+        </MenuItems>
+      </Menu>
+    </div>
+  )
+}
