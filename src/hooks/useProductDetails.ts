96c22cd447b44734e169b3fcc85280fa8fc09ee1
ekaterinailiushenko <kkrus.9725@gmail.com>
2025-02-14 12:01:46 +0100

    feat: add useProductDetails hook for handling new mock products data

---
+++ src/hooks/useProductDetails.ts
@@ -0,0 +1,21 @@
+import { useTranslation } from 'react-i18next'
+
+import type { Product } from '../api/types'
+import { formatPrice, formatWeight } from '../utilities'
+
+export const useProductDetails = ({ product }: { product: Product }) => {
+  const { t } = useTranslation()
+
+  const isNewProduct = product.rating === 0
+  const isProductByPiece = product.weight !== undefined
+
+  const priceText = isProductByPiece
+    ? formatPrice(product.pricePerProduct)
+    : t('products.pricePerKg', { price: formatPrice(product.pricePerProduct) })
+
+  const stockText = isProductByPiece
+    ? t('products.amountInStockPer.piece', { stock: product.stock })
+    : t('products.amountInStockPer.kg', { stock: formatWeight(product.stock) })
+
+  return { isNewProduct, priceText, stockText }
+}
